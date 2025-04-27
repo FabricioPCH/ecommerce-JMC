e87a7e757fed0b8223af6893ee08306eb8b2921f
FabricioPCH <bricos404030@live.com>
2025-04-26 21:36:25 -0500

    Add .gitattributes file to normalize line endings

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,22 +15,29 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
+        'google_id',
+        'avatar',
+        'phone',
+        'address',
+        'last_login_at'
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'google_id',
     ];
 
     /**
@@ -42,7 +49,48 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'last_login_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Check if user is admin
+     *
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Check if user is employee
+     *
+     * @return bool
+     */
+    public function isEmployee(): bool
+    {
+        return $this->role === 'employee';
+    }
+
+    /**
+     * Check if user is client
+     *
+     * @return bool
+     */
+    public function isClient(): bool
+    {
+        return $this->role === 'client';
+    }
+
+    /**
+     * Check if user can login with Google
+     *
+     * @return bool
+     */
+    public function canLoginWithGoogle(): bool
+    {
+        return $this->role === 'client';
     }
 }
