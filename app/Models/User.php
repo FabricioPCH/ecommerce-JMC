b6b06bde67cdb713035414d750310e1ef31b5cc8
FabricioPCH <bricos404030@live.com>
2025-04-26 21:37:40 -0500

    Normalize line endings

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -92,5 +93,13 @@ class User extends Authenticatable
     public function canLoginWithGoogle(): bool
     {
         return $this->role === 'client';
+    }
+
+    /**
+     * Get the intentions for the user.
+     */
+    public function intentions(): HasMany
+    {
+        return $this->hasMany(Intention::class);
     }
 }
